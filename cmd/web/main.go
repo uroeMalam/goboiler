47a2e41fec26dbba9ebe4012ad2a823869ea5a80
rizqullah sahputra <rizqullahsahputra153@gmail.com>
2024-11-28 10:41:00 +0700

    init project

---
+++ cmd/web/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"markopillow/init/internal/config"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := config.NewFiber()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("hello astronout!")
+	})
+
+	webPort := 3000
+	err := app.Listen(fmt.Sprintf(":%d", webPort))
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
